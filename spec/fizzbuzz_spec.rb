fce58f32914b31c6e315ebe15fa21b21d9ce4f64
hirocaster <hohtsuka@gmail.com>
2014-06-23 23:51:29 +0900

    Add spec problem2 case

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'FizzBuzz' do
+  subject(:fizzbuzz) { FizzBuzz.new }
   describe '#say' do
     context 'Standard FizzBuzz' do
-      subject(:fizzbuzz) { FizzBuzz.new }
       let(:format) do
         {3 => 'Fizz', 5 => 'Buzz'}
       end
@@ -37,6 +37,40 @@ describe 'FizzBuzz' do
           expect(fizzbuzz.say 30, format).to eq 'FizzBuzz'
           expect(fizzbuzz.say 45, format).to eq 'FizzBuzz'
         end
+      end
+    end
+
+    context 'Extend FizzBuzz format' do
+      let(:format) do
+        {3 => 'Foo', 7 => 'Bar', 11 => 'Baz'}
+      end
+
+      it 'return Foo' do
+        expect(fizzbuzz.say 3, format).to eq 'Foo'
+        expect(fizzbuzz.say 6, format).to eq 'Foo'
+      end
+
+      it 'return Bar' do
+        expect(fizzbuzz.say 7, format).to eq 'Bar'
+        expect(fizzbuzz.say 14, format).to eq 'Bar'
+      end
+
+      it 'return Baz' do
+        expect(fizzbuzz.say 11, format).to eq 'Baz'
+        expect(fizzbuzz.say 22, format).to eq 'Baz'
+      end
+
+      it 'return FooBar' do
+        expect(fizzbuzz.say 21, format).to eq 'FooBar'
+      end
+
+      it 'return BarBaz' do
+        expect(fizzbuzz.say 77, format).to eq 'BarBaz'
+      end
+
+      it 'return BarBaz' do
+        expect(fizzbuzz.say 33, format).to eq 'FooBaz'
+        expect(fizzbuzz.say 99, format).to eq 'FooBaz'
       end
     end
   end
